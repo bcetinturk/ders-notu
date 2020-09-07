441fda27800d8b17e357dbad3ca26433a0365c2d
Barış Çetintürk <baris.cetinturk.ytu@gmail.com>
2020-09-07 23:52:19 +0300

    initial commit

---
+++ src/main/java/com/example/dersnotu/DersnotuApplication.java
@@ -0,0 +1,13 @@
+package com.example.dersnotu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DersnotuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DersnotuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dersnotu/DersnotuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dersnotu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DersnotuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
